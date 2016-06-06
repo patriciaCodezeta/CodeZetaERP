2cf73a6508ebcd6bc03128bc99ee39b884898cfe
Wi <Wi@Zacaras>
2016-06-06 11:07:36 +0200

    primera prueba para git

---
+++ src/codezetaerp/CodeZetaERP.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codezetaerp;
+
+/**
+ *
+ * @author Wi
+ */
+public class CodeZetaERP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Esto es una prueba para GIT
+    }
+    
+}
